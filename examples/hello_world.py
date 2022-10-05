80e4db6822e9fbce477cf970060b26c1430b2bee
stonehippo <stonehippo@gmail.com>
2022-10-05 09:36:58 -0400

    Modify basic example to use board.UART()

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,9 +1,13 @@
 import board
-import busio
+# import busio if board.UART() is not available
+#import busio
 import serial_lcd_kit
 
-uart = busio.UART(board.TX, board.RX, baudrate=9600)
-lcd = serial_lcd_kit.SerialLCDKit(uart)
+lcd = serial_lcd_kit.SerialLCDKit(board.UART())
+
+# if the above doesn't work, or if you need to use a secondary UART, use busio
+#uart = busio.UART(board.TX, board.RX, baudrate=9600)
+#lcd = serial_lcd_kit.SerialLCDKit(uart)
 
 lcd.clear_display()
 lcd.write("Hello, world!")
